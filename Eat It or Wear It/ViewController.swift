3fd0027af8c2099bfb27fb096a418b2caa054b7b
Nate Tomlinson <natetomlinson@Nate-Tomlinson.local>
2017-01-11 19:35:00 -0800

    Initial Commit

---
+++ Eat It or Wear It/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eat It or Wear It
+//
+//  Created by Nate Tomlinson on 1/11/17.
+//  Copyright © 2017 nate tomlinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Eat It or Wear ItTests/Eat_It_or_Wear_ItTests.swift
@@ -0,0 +1,36 @@
+//
+//  Eat_It_or_Wear_ItTests.swift
+//  Eat It or Wear ItTests
+//
+//  Created by Nate Tomlinson on 1/11/17.
+//  Copyright © 2017 nate tomlinson. All rights reserved.
+//
+
+import XCTest
+@testable import Eat_It_or_Wear_It
+
+class Eat_It_or_Wear_ItTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
